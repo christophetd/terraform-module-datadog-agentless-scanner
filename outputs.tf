1f79ab12f65f561f0400f506b085ee9a2913e1a0
Diogo Pereira <diogo.pereira@datadoghq.com>
2024-04-11 15:02:52 +0100

    Make AWS outputs depend on var `use_aws`

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "vpc" {
   description = "The VPC created for the Datadog agentless scanner"
-  value       = module.aws.vpc.vpc
+  value       = var.use_aws ? module.aws.vpc.vpc : null
 }
 
 output "api_key_secret_arn" {
   description = "The ARN of the secret containing the Datadog API key"
-  value       = module.aws.user_data.api_key_secret_arn
+  value       = var.use_aws ? module.aws.user_data.api_key_secret_arn : null
 }
